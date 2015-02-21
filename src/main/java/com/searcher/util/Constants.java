bf8a166223a8a1f4daf6d77cc64b3765948bef62
Anatoliy Prokhorenko <anatoliy.ck@gmail.com>
2015-02-21 18:41:59 +0200

    Increase count of maximum getting links per page

--- src/main/java/com/searcher/util/Constants.java
+++ src/main/java/com/searcher/util/Constants.java
@@ -3,7 +3,7 @@ package com.searcher.util;
 public final class Constants {
     public static final int ZERO = 0;
     public static final int NUMBER_OF_THREADS = 5;
-    public static final int MAX_LINKS_PER_PAGE = 50;
+    public static final int MAX_LINKS_PER_PAGE = 100;
     public static final int HITS_PER_PAGE = 100;
     public static final int RECORDS_PER_PAGE = 10;
     public static final String TEXT = "text";
