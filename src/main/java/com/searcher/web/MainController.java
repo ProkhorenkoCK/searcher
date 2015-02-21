8c238a643f252c3ae85780dfe4b601de7e21ec33
Anatoliy Prokhorenko <anatoliy.ck@gmail.com>
2015-02-21 16:54:45 +0200

    Added expanded logs

--- src/main/java/com/searcher/web/MainController.java
+++ src/main/java/com/searcher/web/MainController.java
@@ -56,26 +56,32 @@ public class MainController {
     @RequestMapping(value = "/search",  method = { RequestMethod.GET, RequestMethod.POST })
     public String searchData(@RequestParam(value = "q", required = false) String query,
                              @RequestParam(value = "page", required = false) Integer page,
-                             Model model) throws IOException, InterruptedException {
-        if (query != null) {
-            List<SearchData> searchDataList = searcher.searchWord(query);
-            if (searchDataList != null && searchDataList.size() > ZERO) {
-                int currentPage = 1;
-                if (page != null && page > ZERO) {
-                    currentPage = page;
+                             Model model) {
+        try {
+            if (query != null) {
+                List<SearchData> searchDataList = searcher.searchWord(query);
+                if (searchDataList != null && searchDataList.size() > ZERO) {
+                    int currentPage = 1;
+                    if (page != null && page > ZERO) {
+                        currentPage = page;
+                    }
+                    int recordsPerPage = RECORDS_PER_PAGE;
+                    int offset = (currentPage - 1) * recordsPerPage;
+                    offset = searchService.getCorrectOffset(searchDataList.size(), offset, recordsPerPage);
+                    int noOfRecords = searchDataList.size();
+                    int noOfPages = (int) Math.ceil(noOfRecords * 1.0 / recordsPerPage);
+
+                    List<SearchData> pageData = searchService.getSearchData(searchDataList, offset, recordsPerPage);
+
+                    model.addAttribute(SEARCH_DATA, pageData);
+                    model.addAttribute(NO_OF_PAGES, noOfPages);
+                    model.addAttribute(CURRENT_PAGE, currentPage);
                 }
-                int recordsPerPage = RECORDS_PER_PAGE;
-                int offset = (currentPage - 1) * recordsPerPage;
-                offset = searchService.getCorrectOffset(searchDataList.size(), offset, recordsPerPage);
-                int noOfRecords = searchDataList.size();
-                int noOfPages = (int) Math.ceil(noOfRecords * 1.0 / recordsPerPage);
-
-                List<SearchData> pageData = searchService.getSearchData(searchDataList, offset, recordsPerPage);
-
-                model.addAttribute(SEARCH_DATA, pageData);
-                model.addAttribute(NO_OF_PAGES, noOfPages);
-                model.addAttribute(CURRENT_PAGE, currentPage);
             }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (InterruptedException e) {
+            System.out.println(e.getClass() + " " + e.getMessage());
         }
         return "search";
     }
